bdb1fed08f9365742c9837be85bf7bbb938a6bd3
zhaobo <jakobzhao@gmail.com>
2020-01-28 19:40:51 -0800

    update

--- headless_virus.py
+++ headless_virus.py
@@ -12,6 +12,7 @@ import sqlite3
 
 
 from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
 chrome_options.add_argument("--headless")
 # chrome_options.add_argument("--window-size=1920x1080")
 # driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
