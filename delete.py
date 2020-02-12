f84320c7a5433224a8df2f5063c2be419ff07d90
Bo Zhao <jakobzhao@gmail.com>
2020-02-11 17:25:21 -0800

    update

--- delete.py
+++ delete.py
@@ -6,7 +6,13 @@ if lines[n-1][0:10] == lines[n-2][0:10]:
     lines.pop(n-2)
 pass
 with open("assets/virus2.csv", "w", encoding="utf-8") as fp:
+    start = 10
+    stop = 26
+    id = 0
     for line in lines:
-        fp.write(line)
-
+        if id !=0:
+            fp.write(line[0: 10:] + line[26::])
+        else:
+            fp.write(line)
+        id += 1
 print("finished!")
--- headless_virus.py
+++ headless_virus.py
@@ -168,4 +168,24 @@ with open("assets/virus.csv", "w", encoding="utf-8") as fp:
             fp.write(str(row)[1:len(str(row)) - 1].replace("\'", "").replace("None", "").replace(", ", ",").replace("(null)", "") + "\n")
 
 conn.close()
+
+
+fp = open("assets/virus.csv", "r", encoding="utf-8")
+lines = fp.readlines()
+fp.close()
+n = len(lines)
+if lines[n-1][0:10] == lines[n-2][0:10]:
+    lines.pop(n-2)
+pass
+with open("assets/virus.csv", "w", encoding="utf-8") as fp:
+    start = 10
+    stop = 26
+    id = 0
+    for line in lines:
+        if id !=0:
+            fp.write(line[0: 10:] + line[26::])
+        else:
+            fp.write(line)
+        id += 1
+
 print("finished!")
