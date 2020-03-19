99821cb9c7bbd458b09aad4fb0b2697b377f6900
Bo Zhao <jakobzhao@gmail.com>
2020-03-19 09:10:51 -0700

    update 3/19

--- virus.py
+++ virus.py
@@ -14,21 +14,17 @@ options.add_argument('--no-sandbox')
 # browser = webdriver.Chrome("/usr/bin/chromedriver", options=options)
 # browser = webdriver.Chrome("E:\chromedriver_win32\chromedriver.exe")
 
-#<<<<<<< HEAD
+
 #browser = webdriver.Chrome("/Users/FengyuXu/Desktop/web_crawler/twitter_crawler/chromedriver")
-browser = webdriver.Chrome("/Users/joshuaji/Desktop/chromedriver") #joshua's chromedrive location
+#browser = webdriver.Chrome("/Users/joshuaji/Desktop/chromedriver") #joshua's chromedrive location
 #browser = webdriver.Chrome("C:/workspace/chromedriver.exe")
 #browser = webdriver.Chrome("E:/dev/workspaces/chromedriver.exe")
 
 #browser = webdriver.Chrome("/Users/FengyuXu/Desktop/web_crawler/twitter_crawler/chromedriver")
 #browser = webdriver.Chrome("/Users/joshuaji/Desktop/chromedriver") #joshua's chromedrive location
-#browser = webdriver.Chrome("C:/workspace/chromedriver.exe")
-<<<<<<< HEAD
+browser = webdriver.Chrome("C:/workspace/chromedriver.exe")
 # browser = webdriver.Chrome("E:/dev/workspaces/chromedriver.exe")
-#>>>>>>> 99465fbe3ee6929f37edfce2fcd124c1ab4aa67f
-=======
 #browser = webdriver.Chrome("E:/dev/workspaces/chromedriver.exe")
->>>>>>> 3343809a2b066a2abee0bc1d77862ae98a2cd7b4
 #browser = webdriver.Chrome("/Users/stevenbao/dev/chromedriver")
 
 now = str(datetime.now())
