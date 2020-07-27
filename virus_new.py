d36afe7982dbd7cfa17b132b4462f98c0c8df750
Steven Bao <whichsteven297@gmail.com>
2020-07-27 15:12:43 -0700

    data update 7/27 afternoon

--- virus_new.py
+++ virus_new.py
@@ -11,9 +11,9 @@ import requests
 #browser = webdriver.Chrome("/Users/FengyuXu/Desktop/web_crawler/twitter_crawler/chromedriver")
 #browser = webdriver.Chrome("/Users/joshuaji/Desktop/chromedriver") #joshua's chromedrive location
 #browser = webdriver.Chrome("C:/workspace/chromedriver.exe")
-#browser = webdriver.Chrome("E:/dev/workspaces/chromedriver.exe")
+browser = webdriver.Chrome("E:/dev/workspaces/chromedriver.exe")
 #browser = webdriver.Chrome("C:/Users/Steven/workspaces/chromedriver.exe")
-browser = webdriver.Chrome("/Users/stevenbao/dev/chromedriver")
+#browser = webdriver.Chrome("/Users/stevenbao/dev/chromedriver")
 
 # Variable Preparation
 now = str(datetime.now())
